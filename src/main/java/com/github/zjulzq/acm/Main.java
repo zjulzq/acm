4195433d02a9adc83aed81037fb01f344f4ef158
lizhiqiang.lzq <lizhiqiang.lzq@cainiao.com>
2016-12-04 20:39:40 +0800

    Problem 1001

--- src/main/java/com/github/zjulzq/acm/Main.java
+++ src/main/java/com/github/zjulzq/acm/Main.java
@@ -1,15 +1,25 @@
 package com.github.zjulzq.acm;
 
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.println(a + b);
+        while (scanner.hasNext()) {
+            BigDecimal r = scanner.nextBigDecimal();
+            int n = scanner.nextInt();
+            r = r.pow(n);
+
+            String output = r.stripTrailingZeros().toPlainString();
+            if (output.startsWith("0") && output.length() > 1) {
+                System.out.println(output.substring(1));
+            } else {
+                System.out.println(output);
+            }
+        }
     }
 
 }
