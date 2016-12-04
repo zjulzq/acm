c0e7b6bb2df46009c44eb2497297799ab1f672d7
lizhiqiang.lzq <lizhiqiang.lzq@cainiao.com>
2016-12-04 21:25:52 +0800

    Problem 1003

--- src/main/java/com/github/zjulzq/acm/Main.java
+++ src/main/java/com/github/zjulzq/acm/Main.java
@@ -1,25 +1,31 @@
 package com.github.zjulzq.acm;
 
 
-import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
-            BigDecimal r = scanner.nextBigDecimal();
-            int n = scanner.nextInt();
-            r = r.pow(n);
-
-            String output = r.stripTrailingZeros().toPlainString();
-            if (output.startsWith("0") && output.length() > 1) {
-                System.out.println(output.substring(1));
-            } else {
-                System.out.println(output);
+            String input = scanner.next();
+            if ("0.00".equals(input)) {
+                break;
+            }
+            double target = Double.valueOf(input);
+            int cards = 0;
+            double total = 0;
+            while (true) {
+                cards++;
+                total += 1.0 / (cards + 1);
+                if (total >= target) {
+                    System.out.println(cards + " card(s)");
+                    break;
+                }
             }
         }
     }
+
 
 }
